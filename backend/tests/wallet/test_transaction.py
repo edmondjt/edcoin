f90f6b0f0d0a84a90c45b4fd785ee5529c439713
Tech <tech@Edmonds-MacBook-Pro.local>
2020-10-21 10:33:17 -0700

    test transaction updates

--- backend/tests/wallet/test_transaction.py
+++ backend/tests/wallet/test_transaction.py
@@ -1,3 +1,4 @@
+import pytest
 import time
 
 from backend.wallet.transaction import Transaction
@@ -16,9 +17,49 @@ def test_transaction():
     assert transaction.input['amount'] == sender_wallet.balance
     assert transaction.input['address'] == sender_wallet.address
     assert transaction.input['public_key'] == sender_wallet.public_key
-    
     assert Wallet.verify(
         transaction.input['public_key'],
         transaction.output,
         transaction.input['signature']
+    )
+    
+def test_transaction_exceeds_balance():
+    with pytest.raises(Exception, match='Amount exceeds balance'):
+        Transaction(Wallet(), 'recipient', 9500)
+
+def test_transaction_update_exceeds_balance():
+    sender_wallet = Wallet()
+    transaction = Transaction(sender_wallet, 'recipient', 50)
+    
+    with pytest.raises(Exception, match = 'Amount exceeds balance'):
+        transaction.update(sender_wallet, 'new_recipient', 9500)
+        
+def test_transaction_update():
+    sender_wallet = Wallet()
+    first_recipient = 'first_recipient'
+    first_amount = 50
+    
+    transaction = Transaction(sender_wallet, first_recipient, first_amount)
+    
+    next_recipient = 'next_recipient'
+    next_amount = 75
+    transaction.update(sender_wallet, next_recipient, next_amount)
+    
+    assert transaction.output[next_recipient] == next_amount
+    assert transaction.output[sender_wallet.address] == sender_wallet.balance - first_amount - next_amount
+    assert Wallet.verify(
+        transaction.input['public_key'],
+        transaction.output,
+        transaction.input['signature']
+    )
+    
+    to_first_again_amount = 25
+    transaction.update(sender_wallet, first_recipient, to_first_again_amount)
+    
+    assert transaction.output[first_recipient] == first_amount + to_first_again_amount
+    assert transaction.output[sender_wallet.address] == sender_wallet.balance - first_amount - next_amount - to_first_again_amount
+    assert Wallet.verify(
+    transaction.input['public_key'],
+    transaction.output,
+    transaction.input['signature']
     )
